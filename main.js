38cad81463c90cefce014707ddc5e5db67024bf9
rhdmswls12 <rhgusdnr1293@naver.com>
2023-12-04 18:47:27 +0900

    간단한 누적합 계산

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 let time = [3, 1, 4, 3, 2]
+let sum = 0
 let answer = 0
 
 time = time.sort((a, b) => a - b)
 for (let i=0; i<time.length; i++) {
-  let sum = 0
-  for (let j=0; j<=i; j++) {
-    sum += time[j]
-  }
-  answer += sum
+  sum += time[i] // i번 째 사람이 기다린 총 시간
+  answer += sum // 지금까지 소요된 총 시간
 }
 console.log(answer)
