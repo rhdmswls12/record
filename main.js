78866d8ea687712999d0dac7c13225272139524c
rhdmswls12 <rhgusdnr1293@naver.com>
2023-12-26 18:46:00 +0900

    BigInt

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
-// 심볼(Symbol)
-import apple from './apple.js'
-import { birthKey, emailsKey } from './keys.js'
+// BigInt
 
-const sKey = Symbol('Hello!')
-const user = {
-  key: '일반 정보!',
-  [sKey]: '민감한 정보!'
-}
+console.log(12345677890123456789012345678901234567890)
+console.log(12345677890123456789012345678901234567890n)
+console.log(BigInt('12345677890123456789012345678901234567890'))
 
-console.log(user.key)
-console.log(user['key'])
-console.log(user[sKey])
-console.log(user[Symbol('Hello!')])
-console.log(sKey)
-console.log(typeof sKey)
+const a = 11n
+const b = 22
 
-console.log(apple)
-console.log(Object.keys(apple))
-console.log(apple[birthKey])
-console.log(apple[emailsKey])
+// 숫자 => BigInt
+console.log(a + BigInt(b))
+console.log(typeof (a + BigInt(b)))
+
+// BigInt => 숫자
+console.log(Number(a) + b)
+console.log(typeof(Number(a) + b))
