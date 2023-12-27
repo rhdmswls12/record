c35a171cbbaeaa74a54b2b2cdd239b18bc6fe866
rhdmswls12 <rhgusdnr1293@naver.com>
2023-12-27 17:28:14 +0900

    배열 데이터와 메모리

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 // 불변성 & 가변성
 
-let a = { x: 1 }
-let b = { x: 1 }
+let a = [1, 2, 3]
+let b = a
 
-console.log(a === b) // 데이터를 바꾸기 전이어도 참조형이므로 false 출력
+b[0] = 4
 
-b.x = 2
-
-console.log(b)
-console.log(a)
-
-console.log(1 === 1) // true
-console.log({} === {}) // false
+console.log(b) // [4, 2, 3]
+console.log(a) // [4, 2, 3]
